e6f8795acd8e930c7533f598e2317b9bfd93545e
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2023-04-12 20:28:57 -0700

    Initialize Repo

---
+++ Welcome_Page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(page_title="Spotify Data Science Project", page_icon=":musical_note:")
+st.title("Welcome Page!")
+st.write("This web app is used to display what I've learned and different analytical techniques I have used with the data provided using the Spotify API!")
+
+img = Image.open("spotify_logo.png")
+st.image(img)

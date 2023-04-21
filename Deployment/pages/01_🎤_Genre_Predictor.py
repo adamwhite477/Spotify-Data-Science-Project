e6a09187baf95b0fdf4907298f9d607c8ac3f3b7
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2023-04-20 18:17:16 -0700

    Update requirements fix?

--- Deployment/pages/01_🎤_Genre_Predictor.py
+++ Deployment/pages/01_🎤_Genre_Predictor.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from fastai.vision.all import load_learner
 from PIL import Image
+import pathlib
+import platform
+plt = platform.system()
+if plt == 'Linux': 
+    pathlib.WindowsPath = pathlib.PosixPath
 model = load_learner("model2.pkl")
 
 st.set_page_config(page_title="Genre Predictor", page_icon=":microphone:")

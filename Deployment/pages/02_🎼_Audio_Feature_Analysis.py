35092d2d32ac2a73f02b0b9046488f2f55754950
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2023-04-20 18:18:05 -0700

    Added Pathlib fix to all other streamlit files

--- Deployment/pages/02_🎼_Audio_Feature_Analysis.py
+++ Deployment/pages/02_🎼_Audio_Feature_Analysis.py
@@ -1,5 +1,10 @@
 import streamlit as st
-
+import pathlib
+import platform
+plt = platform.system()
+if plt == 'Linux': 
+    pathlib.WindowsPath = pathlib.PosixPath
+    
 st.set_page_config(page_title="Audio Feature Analysis", page_icon=":musical_score:")
 
 st.write("Audio Feature Page Coming Soon...")
--- Deployment/pages/03_🎸_Song_Recommender.py
+++ Deployment/pages/03_🎸_Song_Recommender.py
@@ -1,5 +1,10 @@
 import streamlit as st
-
+import pathlib
+import platform
+plt = platform.system()
+if plt == 'Linux': 
+    pathlib.WindowsPath = pathlib.PosixPath
+    
 st.set_page_config(page_title="Song Reccomender", page_icon=":guitar:")
 
 st.write("Song Recommender Page Coming Soon...")

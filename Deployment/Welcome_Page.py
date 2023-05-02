b12d947269213c18f2f8c44b7951adee8bbe6208
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2023-05-02 08:58:45 -0700

    More Organization/Starting cleaning webapp

--- Deployment/Welcome_Page.py
+++ Deployment/Welcome_Page.py
@@ -5,7 +5,20 @@ st.set_page_config(page_title="Spotify Data Science Project", page_icon=":musica
 st.title("Welcome Page!")
 st.write("This web app is used to display what I've learned and different analytical techniques I have used with the data provided using the Spotify API!")
 
-img = Image.open("spotify_logo.png")
+img = Image.open("Pictures/spotify_logo.png")
 st.image(img)
-img2 = Image.open("path/Pictures/dataloaders.png")
-st.image(img2)
+
+st.subheader("How it works!")
+
+search_img = Image.open("Pictures/search.png")
+download_img = Image.open("Pictures/download_images.png")
+dataloaders_img = Image.open("Pictures/dataloaders.png")
+export_model_img = Image.open("Pictures/export_model.png")
+load_data_img = Image.open("Pictures/load_data.png")
+train_model_img = Image.open("Pictures/train_model.png")
+verify_images_img = Image.open("Pictures/verify_images.png")
+distribution_img = Image.open("Pictures/distribution.png")
+
+st.write("First, we must search for our Genre pictures so we can download them to train on!")
+
+st.image(search_img)

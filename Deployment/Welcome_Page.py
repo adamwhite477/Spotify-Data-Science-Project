e46eb0d11520327f90fe20fc1ec205ec96b7cfef
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2023-05-02 08:44:20 -0700

    Re-organize github

--- Deployment/Welcome_Page.py
+++ Deployment/Welcome_Page.py
@@ -7,3 +7,5 @@ st.write("This web app is used to display what I've learned and different analyt
 
 img = Image.open("spotify_logo.png")
 st.image(img)
+img2 = Image.open("path/Pictures/dataloaders.png")
+st.image(img2)

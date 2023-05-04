0b8dfbf19207f86b10fedc8a8a1937b3e7ddd185
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2023-05-03 23:00:48 -0700

    Formatting Streamlit

--- Deployment/Welcome_Page.py
+++ Deployment/Welcome_Page.py
@@ -7,18 +7,3 @@ st.write("This web app is used to display what I've learned and different analyt
 
 img = Image.open("Pictures/spotify_logo.png")
 st.image(img)
-
-st.subheader("How it works!")
-
-search_img = Image.open("Pictures/search.png")
-download_img = Image.open("Pictures/download_images.png")
-dataloaders_img = Image.open("Pictures/dataloaders.png")
-export_model_img = Image.open("Pictures/export_model.png")
-load_data_img = Image.open("Pictures/load_data.png")
-train_model_img = Image.open("Pictures/train_model.png")
-verify_images_img = Image.open("Pictures/verify_images.png")
-distribution_img = Image.open("Pictures/distribution.png")
-
-st.write("First, we must search for our Genre pictures so we can download them to train on!")
-
-st.image(search_img)
--- Deployment/pages/01_🎤_Genre_Predictor.py
+++ Deployment/pages/01_🎤_Genre_Predictor.py
@@ -25,3 +25,19 @@ if uploaded_img is not None:
     with st.container():
         st.write("This is a {} album".format(genre))
         st.write("It has a confidence of {:.4f}".format(probs[0]))
+
+
+st.subheader("How it works!")
+
+search_img = Image.open("Pictures/search.png")
+download_img = Image.open("Pictures/download_images.png")
+dataloaders_img = Image.open("Pictures/dataloaders.png")
+export_model_img = Image.open("Pictures/export_model.png")
+load_data_img = Image.open("Pictures/load_data.png")
+train_model_img = Image.open("Pictures/train_model.png")
+verify_images_img = Image.open("Pictures/verify_images.png")
+distribution_img = Image.open("Pictures/distribution.png")
+
+st.write("First, we must search for our Genre pictures so we can download them to train on!")
+
+st.image(search_img)

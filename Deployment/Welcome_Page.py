355b1d46c320f0557075e56098454c235439d177
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2023-10-24 15:46:53 -0700

    Changing deployment

--- Deployment/Welcome_Page.py
+++ Deployment/Welcome_Page.py
@@ -1,12 +1,37 @@
 import streamlit as st
 from PIL import Image
+import pathlib
+from fastai.vision.all import load_learner
+import platform
+
 
 st.set_page_config(page_title="Spotify Data Science Project", page_icon=":musical_note:")
 st.title("Spotify Data Science Project!")
 st.write("**Created By:** Adam White")
 st.write("**Github Repo [HERE](https://github.com/adamwhite477/Spotify-Data-Science-Project)**")
 st.write("This web app is used to display what I've learned and different analytical techniques I have used with the data provided using the Spotify API!")
-st.write("Select a page on the left to check out some of the models I've created!")
+st.write("Try out the model below!")
 
-img = Image.open("Pictures/spotify_logo.png")
-st.image(img)
+
+plt = platform.system()
+if plt == 'Linux': 
+    pathlib.WindowsPath = pathlib.PosixPath
+model = load_learner("model3.pkl")
+
+st.set_page_config(page_title="Genre Predictor", page_icon=":microphone:")
+
+# ---- HEADER -----
+with st.container():
+    # st.subheader("Hello Test")
+    st.title("Test out my model!")
+    st.write("This a model that tries to predict the genre of the image it is given")
+    st.write("Genres it tries to determine: [Rap, Country, Rock]")
+    uploaded_img = st.file_uploader("Upload Image Here", type=["jpg", "png", "jfif", "webp", "jpeg"], accept_multiple_files=False)
+
+if uploaded_img is not None:
+    img = Image.open(uploaded_img)
+    st.image(img)
+    genre,_,probs = model.predict(img)
+    with st.container():
+        st.write("This is a {} album".format(genre))
+        st.write("It has a confidence of {:.4f}".format(probs[0]))
--- Deployment/pages/01_🎤_How_It_Works.py
+++ Deployment/pages/01_🎤_How_It_Works.py
@@ -2,29 +2,14 @@ import streamlit as st
 from fastai.vision.all import load_learner
 from PIL import Image
 import pathlib
-import platform
-plt = platform.system()
-if plt == 'Linux': 
-    pathlib.WindowsPath = pathlib.PosixPath
-model = load_learner("model3.pkl")
 
-st.set_page_config(page_title="Genre Predictor", page_icon=":microphone:")
+
+st.set_page_config(page_title="How It Works!", page_icon=":microphone:")
 
 # ---- HEADER -----
 with st.container():
     # st.subheader("Hello Test")
-    st.title("Test out my model!")
-    st.write("This a model that tries to predict the genre of the image it is given")
-    st.write("Genres it tries to determine: [Rap, Country, Rock]")
-    uploaded_img = st.file_uploader("Upload Image Here", type=["jpg", "png", "jfif", "webp", "jpeg"], accept_multiple_files=False)
-
-if uploaded_img is not None:
-    img = Image.open(uploaded_img)
-    st.image(img)
-    genre,_,probs = model.predict(img)
-    with st.container():
-        st.write("This is a {} album".format(genre))
-        st.write("It has a confidence of {:.4f}".format(probs[0]))
+    st.title("Process of Creating this Model!")
 
 
 st.subheader("How it works!")

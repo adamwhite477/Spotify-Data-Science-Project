68deabf3752ea859b7e73a1e620df3989ec9c506
adamwhite477 <awhite8@zagmail.gonzaga.edu>
2023-10-24 15:54:35 -0700

    Added description text

--- Deployment/Welcome_Page.py
+++ Deployment/Welcome_Page.py
@@ -6,11 +6,20 @@ import platform
 
 
 st.set_page_config(page_title="Spotify Data Science Project", page_icon=":musical_note:")
-st.title("Spotify Data Science Project!")
+st.title(":green[Spotify Data Science Project!]")
 st.write("**Created By:** Adam White")
 st.write("**Github Repo [HERE](https://github.com/adamwhite477/Spotify-Data-Science-Project)**")
-st.write("This web app is used to display what I've learned and different analytical techniques I have used with the data provided using the Spotify API!")
-st.write("Try out the model below!")
+description_text = '''
+                This is the deployed web app of the deep learning model I created using fastAI! This   
+                model attempts to predict the Genre of the user-generated image based on album cover   
+                data extracted directly from the Spotify API. So far, the model can only predict 3   
+                different genres (Rap, Country, Rock), however I am currently working on another model   
+                that will attempt to expand the genre titles without sacrificing accuracy. This was my   
+                first time using an API and creating a Deep Learning model so it is admittedly a bit basic   
+                but it has given me a good foundation on progressing my knowledge in these tremendously!
+'''
+st.markdown(description_text)
+st.write("Try it out below!")
 
 
 plt = platform.system()
@@ -22,8 +31,6 @@ model = load_learner("model3.pkl")
 with st.container():
     # st.subheader("Hello Test")
     st.title("Test out my model!")
-    st.write("This a model that tries to predict the genre of the image it is given")
-    st.write("Genres it tries to determine: [Rap, Country, Rock]")
     uploaded_img = st.file_uploader("Upload Image Here", type=["jpg", "png", "jfif", "webp", "jpeg"], accept_multiple_files=False)
 
 if uploaded_img is not None:
